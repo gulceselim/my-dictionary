d074f5831a59169d19b176da7fd7d293307dd257
gulceselim <selimgulce@hotmail.com>
2021-01-23 16:24:37 +0300

    Homework have been added✔

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    public class MyDictionary<TKey, TValue>
+    {
+        TKey[] _arrayKey;
+        TKey[] _tempArrayKey;
+
+        TValue[] _arrayValue;
+        TValue[] _tempArrayValue;
+
+        public MyDictionary()
+        {
+            _arrayKey = new TKey[0];
+            _arrayValue = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            _tempArrayKey = _arrayKey;
+            _tempArrayValue = _arrayValue;
+
+            int arrayLength = _tempArrayKey.Length;
+
+            if (IsKeyControl(key))
+            {
+                _arrayKey = new TKey[_arrayKey.Length + 1];
+                _arrayValue = new TValue[_arrayValue.Length + 1];
+
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    _arrayKey[i] = _tempArrayKey[i];
+                    _arrayValue[i] = _tempArrayValue[i];
+                }
+
+                _arrayKey[_arrayKey.Length - 1] = key;
+                _arrayValue[_arrayValue.Length - 1] = value;
+            }
+            else
+            {
+                Console.WriteLine($"This key data {key} already exists!");
+            }
+
+        }
+
+        public int Count
+        {
+            get { return _arrayValue.Length; }
+        }
+
+        public void GetDictionary()
+        {
+            for (int i = 0; i < _arrayKey.Length; i++)
+            {
+                Console.WriteLine($"{_arrayKey[i]} - {_arrayValue[i]}");
+
+            }
+        }
+
+        public bool IsKeyControl(TKey key)
+        {
+            for (int i = 0; i < _arrayKey.Length; i++)
+            {
+                if (_arrayKey[i].Equals(key))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> users1 = new MyDictionary<int, string>();
+            users1.Add(1, "Selim");
+            users1.Add(1, "Emre");
+            users1.Add(2, "Mehmet");
+
+
+            users1.GetDictionary();
+        }
+    }
+}
